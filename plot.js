0c93d302f6bcc4bc890408d574ea11d0c99b49db
Paul Schlueter <paulschlueter@Pauls-MacBook-Pro-2.local>
2019-03-30 16:22:52 -0500

    Sex, Length, Weight Plots Done

--- plot.js
+++ plot.js
@@ -1,10 +1,44 @@
-function makeplot() {
-    Plotly.d3.csv("https://raw.githubusercontent.com/pschluet/baby-survey/master/data.csv", function (data) { processData(data) });
+function main() {
+    Plotly.d3.csv("https://raw.githubusercontent.com/pschluet/baby-survey/master/data.csv", function (data) { buildPlots(data) });
 };
 
-function processData(allRows) {
+function buildPlots(allRows) {
 
     console.log(allRows);
+    
+    Plotly.newPlot('boyGirl', createBarChartData(allRows),
+        { title: 'Boy or Girl?' }, {responsive: true});
+
+    Plotly.newPlot(
+        'length', 
+        createHistogramData(allRows, 'Length'),
+        { 
+            title: 'Length',
+            xaxis: {
+                title: {
+                    text: 'Length (in)'
+                }
+            } 
+        },
+        {responsive: true}
+    );
+
+    Plotly.newPlot(
+        'weight', 
+        createHistogramData(allRows, 'Pounds'),
+        { 
+            title: 'Weight',
+            xaxis: {
+                title: {
+                    text: 'Weight (lbs)'
+                }
+            } 
+        }, 
+        {responsive: true}
+    );
+}
+
+function createBarChartData(allRows) {
     var boyCount = 0, girlCount = 0;
 
     for (var i = 0; i < allRows.length; i++) {
@@ -13,22 +47,24 @@ function processData(allRows) {
         if (row['Sex'] == 'boy') boyCount++; else girlCount++;
     }
 
-    x = ['boy', 'girl']
-    y = [boyCount, girlCount]
-
-    console.log('X', x, 'Y', y);
-    makePlotly(x, y);
+    return [{
+        x: ['Boy', 'Girl'],
+        y: [boyCount, girlCount],
+        type: 'bar'
+    }]
 }
 
-function makePlotly(x, y) {
-    var plotDiv = document.getElementById("plot");
-    var data = [{
-        x: x,
-        y: y,
-        type: 'bar'
-    }];
+function createHistogramData(allRows, columnName) {
+    var data = []
+    for (var i = 0; i < allRows.length; i++) {
+        row = allRows[i];
+        data.push(row[columnName])
+    }
 
-    Plotly.newPlot('boyGirl', data,
-        { title: 'What\'s the Baby Gonna Be?' });
-};
-makeplot();
+    return [{
+        x: data,
+        type: 'histogram'
+    }]
+}
+
+main();
